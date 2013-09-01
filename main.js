27dee20b115452163841e7abf8bcad771386b703
Adam Buechler <butchler@gmail.com>
2013-09-01 13:34:40 -0400

    Remove client-side prediction and simplify code a little.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var drag = 0.99                  // Drag from "air friction" (I thought we were 
 var friction = 0.5               // "Friction" from hitting a wall.
 var boost = 5                    // Speed you receive when you "boost".
 var fps = 60                     // Frames per second.
-var networkDelay = 1000           // Milliseconds between network updates.
+var networkDelay = 200           // Milliseconds between network updates.
 var frameDelay = 1000 / fps
 
 // Connect to Firebase.
@@ -38,6 +38,8 @@ function initPlayerState(gameRef, us, doneCallback) {
 
         // Determine what our color should be.
         var colors = ['blue', 'red', 'green', 'purple', 'yellow', 'magenta', 'cyan']
+
+        // Delete all of the colors that are already being used by other players.
         for (player in state) {
             if (state.hasOwnProperty(player)) {
                 var index = colors.indexOf(state[player].color)
@@ -46,7 +48,7 @@ function initPlayerState(gameRef, us, doneCallback) {
             }
         }
 
-        // Find the first color that wasn't deleted.
+        // Take the first color that wasn't deleted.
         var color = null
         for (i in colors) {
             if (colors.hasOwnProperty(i)) {
@@ -55,8 +57,8 @@ function initPlayerState(gameRef, us, doneCallback) {
             }
         }
 
+        // All of the colors are already taken, so we can't join.
         if (color === null) {
-            // All of the colors are already taken, so we can't join.
             console.log("Too many players in game to join.")
             return
         }
@@ -109,6 +111,33 @@ function drawPlayer(player) {
     g.fillRect(player.x, player.y, size, size)
 }
 
+function handleCollisions(gameRef, state) {
+    // For every pair of players:
+    for (player in state) {
+        if (state.hasOwnProperty(player)) {
+            for (otherPlayer in state) {
+                if (state.hasOwnProperty(otherPlayer) && player !== otherPlayer) {
+                    var p = state[player], o = state[otherPlayer]
+
+                    // If squares overlap:
+                    if (p.x < o.x + size && p.x + size > o.x &&
+                        p.y < o.y + size && p.y + size > o.y) {
+                        // Crazy/stupid collision handling!
+                        var k = 1 + 2*Math.random()
+                        p.dx *= -k; p.dy *= -k
+                        o.dx *= -k; o.dy *= -k
+
+                        // Immediately publish states when two players collide.
+                        // TODO: Use update instead of set so that only one network update is sent.
+                        gameRef.child(player).set(p)
+                        gameRef.child(otherPlayer).set(o)
+                    }
+                }
+            }
+        }
+    }
+}
+
 // gameRef is a Firebase reference to the game state, and us is a string
 // containing our player id. gameRef.child(id) will hold our current state.
 function startGame(gameRef, us) {
@@ -140,43 +169,17 @@ function startGame(gameRef, us) {
                 }
             }
 
-            // Collision detection. The only actual privilege that the host
-            // has is that they are the only one that does collision handling.
-            if (us === host) {
-                // For every pair of players:
-                for (player in state) {
-                    if (state.hasOwnProperty(player)) {
-                        for (otherPlayer in state) {
-                            if (state.hasOwnProperty(otherPlayer) && player !== otherPlayer) {
-                                var p = state[player], o = state[otherPlayer]
-                                // If squares overlap:
-                                if (p.x < o.x + size && p.x + size > o.x &&
-                                    p.y < o.y + size && p.y + size > o.y) {
-                                    // Crazy/stupid collision handling!
-                                    var k = 1 + 2*Math.random()
-                                    p.dx *= -k; p.dy *= -k
-                                    o.dx *= -k; o.dy *= -k
-
-                                    // Immediately publish states when two players collide.
-                                    // TODO: Use update instead of set so that only one network update is sent.
-                                    gameRef.child(player).set(p)
-                                    gameRef.child(otherPlayer).set(o)
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            // The only actual privilege that the host has is that they are the
+            // only one that does collision handling.
+            if (us === host)
+                handleCollisions(gameRef, state)
         }, frameDelay)
 
-        setInterval(function() {
-            // Publish our current state every once in a while so that other
-            // players can see it.
-            gameRef.child(us).set(state[us])
-        }, networkDelay)
+        // Publish our current state every once in a while so that other
+        // players can see it.
+        setInterval(function() { gameRef.child(us).set(state[us]) }, networkDelay)
 
         // When the other players publish their states, copy it into our state.
-        var lastUpdate, total = count = 0
         gameRef.on('child_added', function(childSnapshot, prevChildName) {
             var player = childSnapshot.name()
 
@@ -186,8 +189,6 @@ function startGame(gameRef, us) {
 
             var playerState = childSnapshot.val()
             state[player] = playerState
-
-            lastUpdate = Date.now()
 
             console.log(state[player].color, 'player added')
         })
@@ -200,29 +201,6 @@ function startGame(gameRef, us) {
 
             var newPlayerState = childSnapshot.val()
             state[player] = newPlayerState
-
-            // Do client-side prediction. Players send their updated state out
-            // every networkDelay milliseconds, but by the time that update
-            // reaches us, we will already be a couple of milliseconds in the
-            // future, so we need to update the players state by a couple of frames.
-
-            // Find the recent average network delay.
-            var delay = Date.now() - lastUpdate
-            lastUpdate = Date.now()
-            total += delay
-            count += 1
-            var averageDelay = total / count
-            if (count > 50) {
-                total -= averageDelay
-                count -= 1
-            }
-
-            var numFrames = (averageDelay / 2) / frameDelay
-            numFrames = 0
-            console.log(numFrames)
-            for (var i = 0; i < numFrames; i++) {
-                movePlayer(state[player])
-            }
         })
         gameRef.on('child_removed', function(oldChildSnapshot) {
             var player = oldChildSnapshot.name()
@@ -230,6 +208,8 @@ function startGame(gameRef, us) {
             delete state[player]
         })
 
+        // Tell the Firebase server to remove our state when the user
+        // disconnects or closes their tab/window.
         gameRef.child(us).onDisconnect().remove()
 
         canvas.onmousemove = function(e) {
@@ -238,14 +218,14 @@ function startGame(gameRef, us) {
             our.mouseY = e.pageY - canvas.offsetTop
         }
         document.onkeypress = function(e) {
-            // Pressing one of the directional keys "boosts" the player by giving
+             // Pressing one of the directional keys "boosts" the player by giving
             // it a constant speed in that direction, instantly nullifying its
             // previous speed in that direction.
             var key = String.fromCharCode(e.which)
-            if (key === "w" || key === "W") our.dy = -boost
-            else if (key === "a" || key === "A") our.dx = -boost
-            else if (key === "s" || key === "S") our.dy = boost
-            else if (key === "d" || key === "D") our.dx = boost
+            if (key === 'w' || key === 'W') our.dy = -boost
+            else if (key === 'a' || key === 'A') our.dx = -boost
+            else if (key === 's' || key === 'S') our.dy = boost
+            else if (key === 'd' || key === 'D') our.dx = boost
         }
     })
 }
